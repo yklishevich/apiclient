cd769645a60e7b11f28d35a40c9053b7b5f8a1d1
Eugene Klishevich <eklishevich@gmail.com>
2019-10-16 18:29:16 +0300

    small improvements

--- APIClient/Source/Alamofire+Foundation.swift
+++ APIClient/Source/Alamofire+Foundation.swift
@@ -9,48 +9,6 @@
 import Foundation
 import Alamofire
 
-//// MARK: – Base Alamofire.Request
-//
-//public extension Request {
-//    
-//    // MARK: - Object
-//    
-//    static func serializeReponseObjectNatively<T: Decodable>(type: T.Type,
-//                                                             response: HTTPURLResponse?,
-//                                                             data: Data?,
-//                                                             error: Error?) -> Result<T, AFError> {
-//        guard error == nil else {
-//            return .failure(error!)
-//        }
-//        guard let validData = data, validData.count > 0 else {
-//            return .failure(AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength))
-//        }
-//        
-//        let decoder = JSONDecoder()
-//        do {
-//            let typedObject: T = try decoder.decode(T.self, from: validData)
-//            return .success(typedObject)
-//        }
-//        catch let anError as DecodingError {
-//            switch anError {
-//            case DecodingError.dataCorrupted:
-//                return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//            case DecodingError.keyNotFound:
-//                return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//            case DecodingError.typeMismatch:
-//                return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//            case DecodingError.valueNotFound:
-//                return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//            default:
-//                return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//            }
-//        }
-//        catch let anError {
-//            return .failure(AFError.responseSerializationFailed(reason: .jsonSerializationFailed(error: anError)))
-//        }
-//    }
-//}
-
 // MARK: – Alamofire.DataRequest
 
 public extension DataRequest {
@@ -68,8 +26,8 @@ public extension DataRequest {
 }
 
 /// A `ResponseSerializer` that decodes the response data using `JSONDecoder`. By default, a request returning
-/// `nil` or no data is considered an error. However, if the response is has a status code valid for empty responses
-/// (`204`, `205`), then an `nil`  value is returned.
+/// `nil` or no data is considered an error. However, if the response has a status code valid for empty responses
+/// (`204`, `205`), then `nil`  value is returned.
 public final class ResponseDecoder<T>: ResponseSerializer where T: Decodable {
     public let dataPreprocessor: DataPreprocessor
     
